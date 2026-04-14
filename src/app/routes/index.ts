8ad47d49e848f9c7fbdd05cbf19c0817cf62a074
Alamin Mustafa Rahim <asaduzzamanalamin112809@gmail.com>
2026-04-15 01:10:57 +0600

    vote competd

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,10 +4,13 @@ import { authRouter } from "../module/auth/auth.router";
 import { ideaRouter } from "../module/idea/idea.route";
 import { feedbackRouter } from "../module/feedback/feedback.router";
 
+import { voteRouter } from "../module/vote/vote.route";
+
 const router = Router();
 router.use("/auth", authRouter);
 router.use("/category", categoryRouter);
 router.use("/idea", ideaRouter);
 router.use("/feedback", feedbackRouter);
+router.use("/vote", voteRouter);
 
 export const appRouter = router;
