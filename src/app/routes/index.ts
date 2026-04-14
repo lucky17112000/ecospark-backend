ccfaf4d9a3f4e152466de95db09acc4bc5698067
Alamin Mustafa Rahim <asaduzzamanalamin112809@gmail.com>
2026-04-14 22:47:18 +0600

    feedback created

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { categoryRouter } from "../module/category/category.router";
 import { authRouter } from "../module/auth/auth.router";
 import { ideaRouter } from "../module/idea/idea.route";
+import { feedbackRouter } from "../module/feedback/feedback.router";
 
 const router = Router();
 router.use("/auth", authRouter);
 router.use("/category", categoryRouter);
 router.use("/idea", ideaRouter);
+router.use("/feedback", feedbackRouter);
 
 export const appRouter = router;
