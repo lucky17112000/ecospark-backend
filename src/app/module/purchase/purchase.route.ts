b48ce1894971d793bc8d4d3af0b4e331b1777a12
Alamin Mustafa Rahim <asaduzzamanalamin112809@gmail.com>
2026-04-15 02:03:32 +0600

    purchase more validation updated

--- src/app/module/purchase/purchase.route.ts
+++ src/app/module/purchase/purchase.route.ts
@@ -2,9 +2,16 @@ import { Router } from "express";
 import { purchaseController } from "./purchase.controller";
 import { cheakAuth } from "../../midddlware/cheakAuth";
 import { Role } from "../../../generated/prisma/enums";
+import { validateRequest } from "../../midddlware/validateRequest";
+import { purchaseValidationzod } from "./purchase.validate";
 
 const router = Router();
-router.post("/", cheakAuth(Role.USER), purchaseController.createPurchase);
+router.post(
+  "/",
+  validateRequest(purchaseValidationzod),
+  cheakAuth(Role.USER),
+  purchaseController.createPurchase,
+);
 
 router.get("/", cheakAuth(Role.ADMIN), purchaseController.getAllPurchases);
 router.get(
